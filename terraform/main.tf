b47e045ab03284164edd37d658a266a04fa5edbf
Anton Ritter <antonritter85@gmail.com>
2018-06-30 01:54:44 +0300

    Load balancer was configured; instance reddit-app2 was added; comment about this was added in README.md (slide 51)

--- terraform/main.tf
+++ terraform/main.tf
@@ -56,6 +56,50 @@ resource "google_compute_instance" "app" {
   }
 }
 
+resource "google_compute_instance" "app2" {
+  name         = "reddit-app2"
+  machine_type = "g1-small"
+  zone         = "europe-west1-b"
+
+  # определение загрузочного диска
+  boot_disk {
+    initialize_params {
+      image = "${var.disk_image}"
+    }
+  }
+
+  metadata {
+    ssh-keys = "appuser:${file(var.public_key_path)}"
+  }
+
+  tags = ["reddit-app"]
+
+  # определение сетевого интерфейса
+  network_interface {
+    # сеть, к которой присоединить данный интерфейс
+    network = "default"
+
+    # использовать ephemeral IP для доступа из Интернет
+    access_config {}
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "appuser"
+    agent       = "false"
+    private_key = "${file(var.private_key_path)}"
+  }
+
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
+}
+
 resource "google_compute_firewall" "firewall-puma" {
   name = "allow-puma-default"
 
